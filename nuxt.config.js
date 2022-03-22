99c910782ca33db6444b23a524d60b2d350e4d93
CJ Mcclure <callum.mcclure98@gmail.com>
2022-03-22 21:31:51 +0000

    Create the bulk of the website

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,26 +9,22 @@ export default {
     titleTemplate: '%s - airbnbWebsite',
     title: 'airbnbWebsite',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -54,7 +50,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -63,13 +59,12 @@ export default {
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,307 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
+  <v-content>
+    <section id="hero">
+      <v-row no-gutters>
+        <v-img
+          :min-height="'calc(100vh)'"
+          :max-height="'calc(100vh)'"
+          src="https://wheretogowithkids.co.uk/wp-content/uploads/2020/11/chapel-st-leonards-beach-chapel-point-1200x800-1-1024x683.jpg"
+        >
+          <v-theme-provider dark>
+            <v-container fill-height fluid>
+              <v-row
+                align="center"
+                class="white--text mx-auto"
+                justify="center"
+              >
+                <v-col class="white--text text-center" cols="12" tag="h1">
+                  <span
+                    :class="[
+                      $vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2',
+                    ]"
+                    class="font-weight-light"
+                  >
+                    WELCOME TO
+                  </span>
+                  <br />
+                  <span
+                    :class="[
+                      $vuetify.breakpoint.smAndDown ? 'display-3' : 'display-4',
+                    ]"
+                    class="font-weight-black"
+                  >
+                    The Seaside Retreat
+                  </span>
+                </v-col>
+
+                <v-btn
+                  class="align-self-end"
+                  fab
+                  outlined
+                  @click="$vuetify.goTo('#about-me')"
+                >
+                  <v-icon>mdi-chevron-double-down</v-icon>
+                </v-btn>
+              </v-row>
+            </v-container>
+          </v-theme-provider>
+        </v-img>
+      </v-row>
+    </section>
+
+    <section id="about-me">
+      <div class="py-12"></div>
+
+      <v-container class="text-center">
+        <h2 class="display-2 font-weight-bold mb-3">About</h2>
+        <span class="display-1 grey--text text--darken-1 font-weight-bold">
+          Great for family time, relaxing and dog walks.
+        </span>
+
+        <v-responsive class="mx-auto mb-8" width="56">
+          <v-divider class="mb-1"></v-divider>
+
+          <v-divider></v-divider>
+        </v-responsive>
+
+        <v-responsive
+          class="mx-auto title font-weight-light mb-8"
+          max-width="720"
+        >
+          A cosy, self-contained, holiday chalet 3 mins walk from endless,
+          sandy, ‘bucket and spade’ beach. Simple, well-equipped accessible
+          chalet on private site with fishing lake. Own private patio area. One
+          double, one twin bedroom. Lovely kitchen and shower room. On site
+          parking . TV and Internet available
+        </v-responsive>
+
+        <v-row justify="center" align="center">
+          <v-col cols="6" sm="4">
+            <v-img src="/living.png"></v-img>
+          </v-col>
+
+          <v-col cols="6" sm="4">
+            <v-img src="/shower.jpg"> </v-img>
+          </v-col>
+
+          <v-col v-if="$vuetify.breakpoint.mdAndUp" cols="6" sm="4">
+            <v-img src="/kitchen.jpg"> </v-img>
+          </v-col>
+        </v-row>
+
+        <div></div>
+      </v-container>
+
+      <div class="py-12"></div>
+    </section>
+
+    <section id="features" class="grey lighten-3">
+      <div class="py-12"></div>
+
+      <v-container class="text-center">
+        <h2 class="display-2 font-weight-bold mb-3">Locally</h2>
+        <v-responsive class="mx-auto mb-8" width="56">
+          <v-divider class="mb-1"></v-divider>
+
+          <v-divider></v-divider>
+        </v-responsive>
+
+        <v-row class="mx-auto title font-weight-light mb-8">
+          <v-col :cols="12" :sm="6">
+            <div>Beautiful local towns</div>
+            <div>Bus to Skegness - a complete contrast!</div>
+            <div>
+              3 minutes to the beach and North Coast Observatory with great
+              healthy menu and fab coffee
+            </div>
+          </v-col>
+          <v-col v-if="$vuetify.breakpoint.xsOnly">
+            <v-divider></v-divider>
+          </v-col>
+          <v-col :cols="12" :sm="6">
+            <div>Great takeaways delivered to the site</div>
+            <div>Great Sunday roasts at local pub</div>
+            <div>Private fishing lake</div>
+            <div>
+              <a href="https://www.eastfieldspark.co.uk/gallery">
+                https://www.eastfieldspark.co.uk/gallery
+              </a>
+            </div>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col
+            v-for="({ icon, title, text }, i) in features"
+            :key="i"
+            cols="12"
+            md="4"
           >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
+            <v-card class="py-12 px-4" color="grey lighten-5" flat>
+              <v-theme-provider dark>
+                <div>
+                  <v-avatar color="primary" size="88">
+                    <v-icon large v-text="icon"></v-icon>
+                  </v-avatar>
+                </div>
+              </v-theme-provider>
+
+              <v-card-title
+                class="justify-center font-weight-black text-uppercase"
+                v-text="title"
+              ></v-card-title>
+
+              <v-card-text class="subtitle-1" v-text="text"> </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <div class="py-12"></div>
+    </section>
+
+    <section id="stats">
+      <v-parallax
+        :height="$vuetify.breakpoint.smAndDown ? 700 : 500"
+        src="https://www.surf-forecast.com/system/images/14628/large/Chapel-St-Leonards.jpg?1442732213"
+      >
+        <v-container fill-height>
+          <v-row class="mx-auto">
+            <v-col cols="12" md="6">
+              <div class="text-center">
+                <div class="display-2 font-weight-black mb-4">
+                  Price Per Month
+                </div>
+
+                <div class="title font-weight-regular text-uppercase">£400</div>
+              </div>
+            </v-col>
+            <v-col cols="12" md="6">
+              <div class="text-center">
+                <div class="display-2 font-weight-black mb-4">
+                  School Holidays
+                </div>
+
+                <div class="title font-weight-regular text-uppercase">£500</div>
+              </div>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-parallax>
+    </section>
+
+    <section id="blog">
+      <div class="py-12"></div>
+
+      <v-container>
+        <h2 class="display-1 font-weight-bold mb-3 text-uppercase text-center">
+          Why so cheap?
+        </h2>
+
+        <v-responsive class="mx-auto mb-12" width="56">
+          <v-divider class="mb-1"></v-divider>
+
+          <v-divider></v-divider>
+        </v-responsive>
+
+        <v-row>
+          <v-col cols="12" md="4" class="text-center">
+            It is close to Skegness - but not too close- a little piece of
+            heaven It is a simple space but not so much storage so pack light.
+          </v-col>
+          <v-col cols="12" md="4" class="text-center">
+            It is in an undiscovered part of the East Lincolnshire coast so
+            prices are still well within reach.
+          </v-col>
+          <v-col cols="12" md="4" class="text-center">
+            The inside is just lovely, the outside simple and functional - it is
+            not The Ritz but we think you will love it - we certainly do!</v-col
           >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+        </v-row>
+      </v-container>
+
+      <div class="py-12"></div>
+    </section>
+
+    <v-sheet id="contact" color="#333333" dark tag="section" tile>
+      <div class="py-12"></div>
+
+      <v-container>
+        <h2 class="display-2 font-weight-bold mb-3 text-uppercase text-center">
+          Contact Us
+        </h2>
+
+        <v-responsive class="mx-auto mb-12" width="56">
+          <v-divider class="mb-1"></v-divider>
+
+          <v-divider></v-divider>
+        </v-responsive>
+
+        <v-row>
+          <v-col :cols="12" :sm="6">
+            <h2 class="display-1 font-weight-bold mb-3 text-uppercase">
+              Do call, we are happy to answer any questions
+            </h2>
+
+            <div>
+              <div class="font-weight-bold mb-3 title">Chris and Ali</div>
+
+              <a
+                class="font-weight-bold mb-3 title white--text"
+                href="tel:07816 408181"
+                >07816 408181</a
+              >
+              <br />
+              <a
+                class="font-weight-bold mb-3 title white--text"
+                href="mailto:callum.mcclure98@gmail.com"
+                >alimcclure@hotmail.com</a
+              >
+            </div>
+          </v-col>
+
+          <v-col :cols="12" :sm="6">
+            <v-theme-provider light>
+              <form
+                action="mailto:callum.mcclure98@gmail.com"
+                method="POST"
+                enctype="text/plain"
+              >
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field flat label="Name*" solo></v-text-field>
+                  </v-col>
+
+                  <v-col cols="12">
+                    <v-text-field flat label="Email*" solo></v-text-field>
+                  </v-col>
+
+                  <v-col cols="12">
+                    <v-text-field flat label="Subject*" solo></v-text-field>
+                  </v-col>
+
+                  <v-col cols="12">
+                    <v-textarea flat label="Message*" solo></v-textarea>
+                  </v-col>
+
+                  <v-col class="mx-auto" cols="auto">
+                    <v-btn type="submit" value="Send" color="accent" x-large>
+                      Submit
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </form>
+            </v-theme-provider>
+          </v-col>
+        </v-row>
+      </v-container>
+
+      <div class="py-12"></div>
+    </v-sheet>
+  </v-content>
 </template>
+<style scoped>
+a {
+  word-break: break-all;
+}
+</style>
